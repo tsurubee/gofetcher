2a6b99b903db454f34d4568bb2f44acb53e3e52e
tsurubee <29603512+tsurubee@users.noreply.github.com>
2018-10-21 20:38:31 +0900

    Add token authentication

---
+++ middleware/token_auth.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+	"github.com/tsurubee/gofetcher/config"
+)
+
+func TokenAuth(next http.Handler, c *config.Config) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("GOFETCHER-API-TOKEN")
+
+		for _, t := range c.Token {
+			if token == t {
+				next.ServeHTTP(w, r)
+			}
+		}
+		http.Error(w, "Forbidden", http.StatusForbidden)
+	})
+}
